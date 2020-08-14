c8c93f50825310d95fbff20d27bfa5d2ac4987f7
Kenneth T. Barrett <kennethtbarrett@criptext.com>
2020-08-14 10:19:32 -0500

    MVP

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -1,13 +1,19 @@
+# Nodes are rooms
+# Edges are pathways between rooms
+
+
 from room import Room
 from player import Player
 from world import World
 
 import random
 from ast import literal_eval
+from util import Stack
 
 # Load world
 world = World()
-
+# Instantiate our player.
+player = Player(world.starting_room)
 
 # You may uncomment the smaller graphs for development and testing purposes.
 # map_file = "maps/test_line.txt"
@@ -17,7 +23,7 @@ world = World()
 map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
-room_graph=literal_eval(open(map_file, "r").read())
+room_graph = literal_eval(open(map_file, "r").read())
 world.load_graph(room_graph)
 
 # Print an ASCII map
@@ -25,10 +31,54 @@ world.print_rooms()
 
 player = Player(world.starting_room)
 
-# Fill this out with directions to walk
-# traversal_path = ['n', 'n']
+# Our traversal path.
 traversal_path = []
+# Track the paths traversed using a Stack.
+paths = Stack()
+# Visited is a dictionary of rooms already traveled.
+visited = set()
 
+
+def shortest_path(direction):
+    """Helper function to determine the inverse direction."""
+    if direction == "n":
+        return "s"
+    elif direction == "s":
+        return "n"
+    elif direction == "e":
+        return "w"
+    elif direction == "w":
+        return "e"
+
+
+# Ensure we haven't visited all rooms.
+while len(visited) < len(world.rooms):
+    # Get the available exits.
+    exits = player.current_room.get_exits()
+    path = []
+
+    # Check each available exit. If the exit existed and wasn't in set, append
+    # to our path.
+    for exit in exits:
+        if (exit is not None and
+           player.current_room.get_room_in_direction(exit) not in visited):
+                path.append(exit)
+    # Add the current room to our set of visited rooms.
+    visited.add(player.current_room)
+
+    # If we have a path, make a random move based upon our path length.
+    if len(path) > 0:
+        move = random.randint(0, len(path)-1)
+        # Push our path at randomly selected index from Stack.
+        paths.push(path[move])
+        player.travel(path[move])  # Make the specified move.
+        traversal_path.append(path[move])  # Append to traversal path.
+    else:  # If there's no path...
+        end = paths.pop()  # Pop from stack
+        # Travel to the inverse path based upon `end`
+        player.travel(shortest_path(end))
+        # Append to traversal path.
+        traversal_path.append(shortest_path(end))
 
 
 # TRAVERSAL TEST
@@ -45,7 +95,6 @@ if len(visited_rooms) == len(room_graph):
 else:
     print("TESTS FAILED: INCOMPLETE TRAVERSAL")
     print(f"{len(room_graph) - len(visited_rooms)} unvisited rooms")
-
 
 
 #######
